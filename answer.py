4479c58d53b0250c6aea414555b0dfab96a93e08
e9gsrivastava <153479304+e9gsrivastava@users.noreply.github.com>
2024-01-02 23:52:34 +0530

    blacked and pylinted

--- answer.py
+++ answer.py
@@ -1,18 +1,12 @@
 """this is to find 2**n where n is a very large no """
+from solver import solver
 
 
-def solver(n):
-    """this func does above task"""
-    num = [2]
-    for _ in range(n - 1):
-        remainder = 0
-        for i in reversed(range(len(num))):
-            temp = num[i] * 2 + remainder
-            num[i], remainder = temp % 10, temp // 10
-            if remainder > 0 and i == 0:
-                num.insert(0, remainder)
-    return sum(num)
+def answer():
+    """this func calls generic func to get the value of 2**100"""
+
+    return solver(100)
 
 
 if __name__ == "__main__":
-    print(solver(100))
+    print(answer())
