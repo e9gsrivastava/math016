52196c30d45216d393c72e165ff48f98e9d5cce0
e9gsrivastava <153479304+e9gsrivastava@users.noreply.github.com>
2024-01-02 23:51:11 +0530

    blacked and pylinted

--- answer.py
+++ answer.py
@@ -1,2 +1,18 @@
-def answer():
-    return
+"""this is to find 2**n where n is a very large no """
+
+
+def solver(n):
+    """this func does above task"""
+    num = [2]
+    for _ in range(n - 1):
+        remainder = 0
+        for i in reversed(range(len(num))):
+            temp = num[i] * 2 + remainder
+            num[i], remainder = temp % 10, temp // 10
+            if remainder > 0 and i == 0:
+                num.insert(0, remainder)
+    return sum(num)
+
+
+if __name__ == "__main__":
+    print(solver(100))
