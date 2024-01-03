354aba87d9e8821496c25671acadd7cb755cf3ad
gaurav srivastava <gaurav.srivastava@enine.school>
2024-01-03 09:06:43 +0530

    blacked and pylinted

--- answer.py
+++ answer.py
@@ -5,7 +5,7 @@ from solver import solver
 def answer():
     """this func calls generic func to get the value of 2**100"""
 
-    return solver(100)
+    return solver(1000)
 
 
 if __name__ == "__main__":
